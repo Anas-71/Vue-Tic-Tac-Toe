5a8811dfe852d5b36b32925576d62b7372e77945
Anas-71 <120765866+Anas-71@users.noreply.github.com>
2024-06-22 18:16:11 +0200

    Add files via upload

---
+++ src/components/Results.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="overview" :class="winner === 'X' ? 'blue' : 'red'">
+    {{ winner === 'X' ? 'BLUE' : 'RED' }} WINS!
+  </div>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    winner: {
+      type: String,
+      required: true,
+    }
+  });
+</script>
+
+<style>
+  .red {
+    background-color: #F98756;
+  }
+  .blue {
+    background-color: #4A8EF5;
+  }
+</style>
---
+++ src/components/Score.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="score-container">
+    <div>
+      <h2>Players Move: {{ player }}</h2>
+      <div>
+        <div class="red-score">Red: {{ redWins }} won.</div>
+        <div class="blue-score">Blue: {{ blueWins }} won.</div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+
+  import { defineProps } from 'vue';
+
+  const props = defineProps({
+    player: {
+      type: String,
+      required: true,
+    },
+    redWins: {
+      type: Number,
+      required: true,
+    },
+    blueWins: {
+      type: Number,
+      required: true,
+    },
+  });
+</script>
+
+<style scoped>
+</style>
